f6ab641149335eba60128e1ccbe3a86eabb00886
MuratO.Edutec <muratedutec@gmail.com>
2022-12-14 19:46:58 +0100

    JDBC 2.gun

--- src/test/java/JDBC01_Query02.java
+++ src/test/java/JDBC01_Query02.java
@@ -9,17 +9,38 @@ public class JDBC01_Query02 {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys?serverTimezone=UTC", "root", "1234");
 
         Statement st = con.createStatement();
-        ResultSet veri = st.executeQuery("Select isim, maas FROM calisanlar where sehir= 'Ankara' order by maas desc;");
+
+        ResultSet veri = st.executeQuery("Select isim, maas, sehir FROM calisanlar where sehir= 'Ankara' order by maas desc;");
 
         while (veri.next()) {
-            System.out.println(veri.getString("isim") + " - " + veri.getInt("maas"));
-
-            /* con.close();
-            st.close();
-            veri.close();
+            System.out.println(veri.getString("isim") + "\n" + veri.getInt("maas") + "\n" + veri.getString("sehir"));
 
 
-             */
+            System.out.println("=== * * * ==== * * * ==== * * * ==== * * * ==== * * * ==== * * * ==== ");
+
+        }
+         /*=======================================================================
+          ORNEK3: Maasi en yuksek 3 kisinin adini, yasadigi sehri ve maasini
+           maas sirali listeyiniz.
+        ========================================================================*/
+        String sorgu="Select isim, sehir, maas FROM calisanlar ORDER BY maas desc LIMIT 3";
+
+        ResultSet veri2=st.executeQuery(sorgu);
+
+
+        while(veri2.next()) {
+            System.out.println(veri2.getString(1) + " " + veri2.getString(2) + " " + veri2.getInt(3));
+        }
+
+
+        /* con.close();
+        st.close();
+        veri.close();
+        veri2.close();
+        */
+
+
     }
     }
-}
+
+
